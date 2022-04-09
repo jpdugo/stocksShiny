dda16d2b0211a128892058a2d84aca8209fc2d25
Juan Patricio Dugo <juanpatriciodugo@gmail.com>
2022-04-09 19:55:39 -0300

    Improved documentation

--- R/app.R
+++ R/app.R
@@ -1,37 +1,35 @@
-#' Title
+#' Run stock application.
 #'
-#' @param ... 
+#' This application let's the user download information about sp500 tickers and
+#' take a look at different metrics and indicators.
+#' 
+#' @param ... Additional arguments to pass to \code{shinyApp()}
+#' @import shiny.semantic
 #'
-#' @return
 #' @export
-#'
-#' @examples
-run_app <- function() {
-
-ui <- semanticPage(
-  shinyjs::useShinyjs(),
-  sidebar_layout(
-    sidebar_panel(
-      pickStockUI("picker")
-    ),
-    main_panel(uiOutput('stock_tabs'))
+run_app <- function(...) {
+  ui <- semanticPage(
+    shinyjs::useShinyjs(),
+    sidebar_layout(
+      sidebar_panel(
+        pickStockUI("picker")
+      ),
+      main_panel(uiOutput("stock_tabs"))
+    )
   )
-)
-server <- function(input, output, session) {
-  ticker <- pickStockServer("picker", sp500nms)
+  server <- function(input, output, session) {
+    ticker <- pickStockServer("picker", sp500nms)
 
-  observeEvent(ticker(),{
-    output$stock_tabs <- renderUI({
-      tabset(tabs = ticker() %>%
-               map(~ list(menu = .x, content = tickerInfoUI(.x))))
+    observeEvent(ticker(), {
+      output$stock_tabs <- renderUI({
+        tabset(tabs = ticker() %>%
+          map(~ list(menu = .x, content = tickerInfoUI(.x))))
+      })
+
+      for (i in ticker()) {
+        tickerInfoServer(i)
+      }
     })
-    
-    for (i in ticker()) {
-      tickerInfoServer(i)
-    }
-  })
-  
-  
-}
-shinyApp(ui, server, ...)
+  }
+  shinyApp(ui, server, ...)
 }
