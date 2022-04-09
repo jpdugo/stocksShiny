f0c10714a19dd35bac50b25e50eff98f54b7ff81
Juan Patricio Dugo <juanpatriciodugo@gmail.com>
2022-04-09 20:37:55 -0300

    documented get_adjusted_prices and get_table_finviz

--- R/get_table_finviz.R
+++ R/get_table_finviz.R
@@ -1,17 +1,19 @@
+#' Get table from finviz
+#' 
+#' Download the table with indicators located below the graph in finviz
+#'
+#' @param ticker a valid ticker name 
+#'
+#' @return tibble
+#' @export
+#' 
+#' @importFrom magrittr %>%
+#'
+#' @examples
+#'get_table_finviz('ATVI')
 get_table_finviz <- function(ticker) {
-  link <- str_c("https://finviz.com/quote.ashx?t=", ticker)
-  url <- read_html(link)
-  tbls <- url %>% html_table()
+  link <- stringr::str_c("https://finviz.com/quote.ashx?t=", ticker)
+  url <- rvest::read_html(link)
+  tbls <- url %>% rvest::html_table()
   tbls[[9]]
-  
-  # x <-   
-  # map_dfc(seq(1, 12, 2), ~ tbl[, c(.x, .x + 1)] %>%
-  #     set_names(c("a", "b")) %>%
-  #     pivot_wider(names_from = "a", values_from = "b", values_fn = list))
-  # 
-  # tbl <- x
-  # walk(names(x), ~ {
-  #   tbl <<- tbl %>% unnest_wider(sym(.x), names_sep = "_")
-  # })
-  # tbl %>% mutate(ticker = str_match(link, "=(.*)$")[[2]], .before = "Index")
 }
