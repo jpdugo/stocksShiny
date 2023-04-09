fe5ae2a27673d58480e376a41026ad99463ff8b9
Juan Patricio Dugo <98497626+jpdugo@users.noreply.github.com>
2023-04-09 19:00:25 -0300

    lint

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -3,8 +3,7 @@ box::use(
   testthat[...],
 )
 box::use(
-  app/main[...],
-  app/logic/tables[xts_to_tibble],
+  app / logic / tables[xts_to_tibble],
   xts[...],
   zoo[index, coredata]
 )
@@ -17,19 +16,19 @@ test_that("xts_to_tibble converts xts objects to tibbles", {
   mat <- matrix(rnorm(50), ncol = 5)
   colnames(mat) <- c("col1", "col2", "col3", "col4", "col5")
   rownames(mat) <- format(Sys.Date() - 0:9, "%Y-%m-%d")
-  
+
   # Convert matrix to xts object
   xts_obj <- xts(mat, order.by = as.Date(rownames(mat), format = "%Y-%m-%d"))
-  
+
   # Print the xts object
   xts_obj
-  
+
   # Convert xts object to tibble
   tib_obj <- xts_to_tibble(xts_obj)
-  
+
   # Check if class of the output is a tibble
   expect_s3_class(tib_obj, "tbl_df")
-  
+
   # Check if the column names match
   expect_equal(colnames(tib_obj[, -1]), colnames(xts_obj))
 })
